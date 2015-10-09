b02b7ff018f520c5de10d640b7a6324abc8e5707
iparedes <iparedes@gmail.com>
2015-10-09 10:03:07 +0400

    Added positions during population

--- splora/models.py
+++ splora/models.py
@@ -1,4 +1,5 @@
 import sys
+import logging
 
 from django.db import models
 from __builtin__ import True
@@ -6,6 +7,9 @@ from __builtin__ import True
 
 from numpy import random
 import numpy
+
+logger=logging.getLogger(__name__)
+
 
 # Create your models here.
 
@@ -31,7 +35,6 @@ class StarSequence(models.Model):
     maxtemp=models.FloatField(default=0)
     minr=models.FloatField(blank=True,default=0)
     maxr=models.FloatField(blank=True,default=0)
-
     desc=models.CharField(max_length=128,default='')
     freq=models.FloatField(default=0)
     
@@ -43,22 +46,58 @@ class StarDensity(models.Model):
     factor=models.FloatField(default=0)
     ident=models.CharField(max_length=24,default='')
     
-    
     def  __unicode__(self):
         return self.ident
     
+    
+class Position(models.Model):
+    x=models.IntegerField(default=0)
+    y=models.IntegerField(default=0)
+    z=models.IntegerField(default=0)
+       
+    def randomPos(self,mini,maxi):
+        self.x=random.randint(mini,maxi)
+        self.y=random.randint(mini,maxi)
+        self.z=random.randint(mini,maxi)
+           
+    def fromTupla(self,t):
+        self.x=t[0]
+        self.y=t[1]
+        self.z=t[2]
+        return self
+    
+    def __add__(self,other):
+        A=Position(self.x+other.x,self.y+other.y,self.z+other.z)
+        return A
+    
+    def inbound(self,limit):
+        """Tests if the position coordinates are in [-limit,limit]
+        """
+        return abs(self.x)<=limit and \
+               abs(self.y)<=limit and \
+               abs(self.z)<=limit
+    
+    def __unicode__(self):
+        return "("+str(self.x)+","+str(self.y)+","+str(self.z)+")"
+    
+
 class Sector(models.Model):
     name=models.CharField(max_length=32)
     numstars=models.IntegerField(default=0)
-    
     """ We try to keep a constant amount of stars inside the sector,
     so we introduce a correction factor to reduce the real size of the sector in bigger densities
     and increase its size in lower densities
     """
     density=models.ForeignKey(StarDensity)
+    
     # Sector has cubic shape. Side is the side of the cube
     side=1000
-    
+    #number of stars while populating
+    #it is used to avoid multiple queries during position generation
+    starsnow=0
+    _positions=[]
+    _mindelta=0
+    _maxdelta=0
     
     def populate(self):
         try:
@@ -67,21 +106,81 @@ class Sector(models.Model):
             factor=self.density.factor
             mu=density*(self.side*factor)**3
             sigma=mu/5
+            # Watch here!!!!!
             self.numstars=int(random.normal(mu,sigma,1)[0])
+            self.numstars=10
+            
+            self._mindelta=Constant.objects.filter(name="MinDelta")[0].value
+            self._maxdelta=Constant.objects.filter(name="MaxDelta")[0].value
+            self._starindependece=Constant.objects.filter(name='StarIndependence')[0].value
+    
+            
             # Save after setting the number of stars
             self.save()
-            cont=0
-            while cont<self.numstars:
+            logger.debug("Populating "+str(self.numstars)+" suns")
+            self.starsnow=0
+            while self.starsnow<self.numstars:
                 S=Sun() 
                 S.rand(self)
+                logger.debug("Created sun "+S.name)
+                p=self.newpos()
+                S.pos=p
                 S.save()
-                cont=cont+1
-                
+                self.starsnow=self.starsnow+1              
             return self.numstars
         except:
             print "Unexpected error:", sys.exc_info()[0]
             raise
         
+    def newpos(self):
+        """Returns a new Position for a solar system
+            The position can be independent or belonging to a cluster
+        """        
+        n=random.random()
+        p=Position()
+        if (n<self._starindependece) or self.starsnow==0:
+            maxi=int(self.side/2)
+            mini=-maxi
+            p=Position()
+            p.randomPos(mini, maxi)
+            tupla=(p.x,p.y,p.z)
+            while tupla in self._positions:
+                p.randomPos(mini, maxi)
+                tupla=(p.x,p.y,p.z)
+            self._positions.append(tupla)
+        else:
+            # Star belongs to a cluster
+            valido=False
+            while not valido:
+                # We're gonna select a random star, calculate a new position near it
+                # and test if there is empty space
+                # We calculate the new position adding deltas to each coordinate
+                # This means that the new position could end out of bounds
+                # This loop forces the position to be inside the sector bounds
+                bounded=False
+                while not bounded:
+                    i=random.choice(len(self._positions),1)[0]
+                    b=self._positions[i]
+                    q=Position()
+                    q.fromTupla(b)
+                    d=Position()
+                    d.fromTupla((self.delta(),self.delta(),self.delta()))
+                    p=q+d
+                    if p.inbound(self.side):
+                        bounded=True
+                if not (p.x,p.y,p.z) in self._positions:
+                    valido=True
+        
+        self._positions.append((p.x,p.y,p.z))
+        p.save()
+        return p  
+                
+    def delta(self):
+        delta=random.randint(self._mindelta,self._maxdelta)
+        if random.random()<0.5:
+            delta=-delta
+        return delta
+
 
     def addsun(self):      
         return 1
@@ -100,7 +199,9 @@ class Sun(models.Model):
     luminosity=models.FloatField()
     temperature=models.FloatField()
     sector=models.ForeignKey(Sector)
+    pos=models.OneToOneField(Position)
     
+
     def rand(self,sector):
         try:
             ismain=Constant.objects.filter(name="IsMainSequence")[0].value
@@ -192,24 +293,10 @@ class Sun(models.Model):
                 self.name=nombre
                 valido=True
         return nombre
+        
 
     
     def __unicode__(self):
         return self.name
     
-    
-class Position(models.Model):
-    x=models.IntegerField(default=0)
-    y=models.IntegerField(default=0)
-    z=models.IntegerField(default=0)
-    
-    sun=models.OneToOneField(Sun)
-    
-    def randomPos(self,mini,maxi):
-        self.x=random.randint(mini,maxi)
-        self.y=random.randint(mini,maxi)
-        self.z=random.randint(mini,maxi)
-    
-    def __unicode__(self):
-        return "("+str(self.x)+","+str(self.y)+","+str(self.z)+")"
     
